a9471d078705aa0de153205d1f85015a46247553
unknown <oktariorifqi@gmail.com>
2021-12-07 14:22:08 -0800

    add vendor

---
+++ app/Views/template/navbar.php
@@ -0,0 +1,5 @@
+<ul class="nav m-2 justify-content-end nav-pills">
+  <li class="nav-item">
+    <a class="nav-link active" aria-current="page" href="<?= base_url(); ?>/login">Log in</a>
+  </li>
+</ul>
